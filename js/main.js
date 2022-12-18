676ae384e515314e6d58b616f8848e7f8808beee
Sandsak <117777158+Sandsak@users.noreply.github.com>
2022-12-18 21:46:56 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Add smooth scrolling on all links inside the navbar
+$("#nav-sandsak a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        $('html, body').animate({
+            scrollTop: ($(hash).offset().top+1)
+        }, 600);
+    }
+});
