0f34236270ee9e626d69f32d23cae8fd34d70b9f
Sandsak <117777158+Sandsak@users.noreply.github.com>
2022-12-21 19:30:30 +0100

    Cookies Google map warning + css update

--- js/main.js
+++ js/main.js
@@ -16,3 +16,10 @@ $("#nav-sandsak a").on('click', function (event) {
         }, 600);
     }
 });
+
+// Load Google map
+$('#btn-gmap').click(function() {
+    $("#gmap").attr("src", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d82898.44843563535!2d6.081117396484366!3d49.51139079471845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47954823e01226b9%3A0xf42d4ab785cec39a!2sSandsak%20Muaythaiplaza2004%20Boxing%20Club%20Asbl%20Asbl!5e0!3m2!1sen!2slu!4v1648918982747!5m2!1sen!2slu");
+    $("#gmap").css("display", "block");
+    $("#block-gmap").css("display", "none");
+});
